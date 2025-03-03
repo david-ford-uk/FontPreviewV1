91adfac106bbe62c953d721410d25cd8ea5138b4
David Ford <110401553+david-ford-uk@users.noreply.github.com>
2025-03-03 08:39:30 +0000

    Add project files.

---
+++ FontPreviewV1/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        flowPanel = New FlowLayoutPanel()
+        txtInput = New TextBox()
+        btnDisplay = New Button()
+        Label1 = New Label()
+        chkAlwaysOnTop = New CheckBox()
+        Label2 = New Label()
+        txtSize = New TextBox()
+        SuspendLayout()
+        ' 
+        ' flowPanel
+        ' 
+        flowPanel.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        flowPanel.AutoScroll = True
+        flowPanel.Location = New Point(3, 38)
+        flowPanel.Name = "flowPanel"
+        flowPanel.Size = New Size(699, 422)
+        flowPanel.TabIndex = 0
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        txtInput.Location = New Point(79, 9)
+        txtInput.Name = "txtInput"
+        txtInput.Size = New Size(362, 23)
+        txtInput.TabIndex = 1
+        txtInput.Text = "Pack my box with five dozen liquor jugs."
+        ' 
+        ' btnDisplay
+        ' 
+        btnDisplay.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnDisplay.Location = New Point(634, 3)
+        btnDisplay.Name = "btnDisplay"
+        btnDisplay.Size = New Size(68, 33)
+        btnDisplay.TabIndex = 2
+        btnDisplay.Text = "show me"
+        btnDisplay.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(5, 12)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(68, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "Sample text"
+        ' 
+        ' chkAlwaysOnTop
+        ' 
+        chkAlwaysOnTop.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        chkAlwaysOnTop.AutoSize = True
+        chkAlwaysOnTop.Location = New Point(525, 11)
+        chkAlwaysOnTop.Name = "chkAlwaysOnTop"
+        chkAlwaysOnTop.Size = New Size(103, 19)
+        chkAlwaysOnTop.TabIndex = 4
+        chkAlwaysOnTop.Text = "Always on Top"
+        chkAlwaysOnTop.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Label2.AutoSize = True
+        Label2.Location = New Point(447, 13)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(27, 15)
+        Label2.TabIndex = 5
+        Label2.Text = "Size"
+        ' 
+        ' txtSize
+        ' 
+        txtSize.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        txtSize.Location = New Point(480, 9)
+        txtSize.Name = "txtSize"
+        txtSize.Size = New Size(39, 23)
+        txtSize.TabIndex = 6
+        txtSize.Text = "18"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(705, 462)
+        Controls.Add(txtSize)
+        Controls.Add(Label2)
+        Controls.Add(chkAlwaysOnTop)
+        Controls.Add(Label1)
+        Controls.Add(btnDisplay)
+        Controls.Add(txtInput)
+        Controls.Add(flowPanel)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MinimumSize = New Size(721, 501)
+        Name = "Form1"
+        Text = "Font Samples for instaled fonts v1.2"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents flowPanel As FlowLayoutPanel
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents chkAlwaysOnTop As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSize As TextBox
+
+End Class
---
+++ FontPreviewV1/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Drawing.Text
+Imports System.Windows.Forms
+
+Public Class Form1
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        ' Set cursor to busy
+        Me.Cursor = Cursors.WaitCursor
+
+        ' Clear previous entries
+        flowPanel.Controls.Clear()
+        flowPanel.FlowDirection = FlowDirection.TopDown ' Ensure vertical stacking
+        flowPanel.WrapContents = False
+
+        ' Get user text
+        Dim userText As String = txtInput.Text
+        Dim fonts As New InstalledFontCollection()
+
+        ' Get user-defined font size (with error handling)
+        Dim fontSizeUser As Integer
+        If Not Integer.TryParse(txtSize.Text, fontSizeUser) OrElse fontSizeUser <= 0 Then
+            fontSizeUser = 18 ' Default to 18 if input is invalid
+            txtSize.Text = 18
+        End If
+
+        For Each fontFamily As FontFamily In fonts.Families
+            ' Create a panel to contain the sample and font name
+            Dim rowPanel As New Panel()
+            rowPanel.AutoSize = True
+            rowPanel.Dock = DockStyle.Top ' Ensures each entry is on a new line
+
+            ' Label for sample text
+            Dim lblSample As New Label()
+            lblSample.AutoSize = True
+            lblSample.Text = userText & " "
+            Try
+                lblSample.Font = New Font(fontFamily, fontSizeUser)
+            Catch ex As Exception
+                lblSample.Font = New Font("Arial", fontSizeUser) ' Fallback font
+            End Try
+
+            ' Label for font name
+            Dim lblFontName As New Label()
+            lblFontName.AutoSize = True
+            lblFontName.Text = fontFamily.Name
+            lblFontName.Font = New Font("Calibri", fontSizeUser, FontStyle.Bold)
+            lblFontName.ForeColor = Color.DarkBlue
+
+            ' Add both labels to the row panel
+            rowPanel.Controls.Add(lblSample)
+            rowPanel.Controls.Add(lblFontName)
+
+            ' Align labels in a single row
+            lblSample.Location = New Point(0, 5)
+            lblFontName.Location = New Point(lblSample.Width + 10, 5)
+
+            ' Add row to main scrollable panel
+            flowPanel.Controls.Add(rowPanel)
+        Next
+
+        ' Restore normal cursor
+        Me.Cursor = Cursors.Default
+    End Sub
+    Private Sub txtSize_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSize.KeyPress
+        ' Allow only numbers, backspace, and delete
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True ' Block invalid input
+        End If
+    End Sub
+    Private Sub txtInput_KeyDown(sender As Object, e As KeyEventArgs) Handles txtInput.KeyDown, txtSize.KeyDown
+        Try
+            If e.KeyCode = Keys.Enter Then
+                e.SuppressKeyPress = True ' Prevents the beep sound when pressing Enter
+                btnDisplay_Click(sender, e) ' Calls the button click event
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub chkAlwaysOnTop_CheckedChanged(sender As Object, e As EventArgs) Handles chkAlwaysOnTop.CheckedChanged
+        If chkAlwaysOnTop.Checked Then
+            Me.TopMost = True ' Keep window always on top
+        Else
+            Me.TopMost = False ' Allow other windows to be on top
+        End If
+    End Sub
+
+End Class
